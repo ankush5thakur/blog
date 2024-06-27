51d5c776cbfdc1dd855478f7f2ae390c256f3381
ANKUSH THAKUR <ankukush05@gmail.com>
2024-06-27 07:30:31 +0530

    header bnn gya

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,24 @@
-import React from 'react'
 
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import DashBoard from './pages/DashBoard.jsx';
+import Sigup from './pages/Sigup';
+import Singnin from './pages/Singnin';
+import Project from './pages/Projects';  
+import Header from './components/Header.jsx';
 export default function App() {
   return (
-    <div className='text-balance text-red-500'>App</div>
-  )
+    <BrowserRouter>
+    <Header/>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/dashboard' element={<DashBoard />} />
+        <Route path='/project' element={<Project />} />
+        <Route path='/sigup' element={<Sigup />} />
+        <Route path='/signin' element={<Singnin />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
