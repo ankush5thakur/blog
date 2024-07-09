ab8809d473e55d914f80cf186bc4d24d100be54d
ankush5thakur <ankushiit21@gmail.com>
2024-07-09 22:57:44 +0530

    updated user data

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,7 +1,10 @@
 import { configureStore,combineReducers } from '@reduxjs/toolkit'
+// import userReducer from './user/userSlice.js'; 
+// Make sure this is the correct path
+// import themeReducer from './theme/themeSlice.js'; 
 import  useReducer  from './user/userSlice.js'
 import {persistReducer} from 'redux-persist'
-// import {persistStore} from 'redux-persist/es/persistReducer'
+import themeReducer from './theme/themeSlice.js'
 import storage from 'redux-persist/lib/storage'
 import persistStore from 'redux-persist/es/persistStore'
 
@@ -9,6 +12,7 @@ import persistStore from 'redux-persist/es/persistStore'
 
 const rootReducer=combineReducers({
   user:useReducer,
+  theme:themeReducer,
 })
 
 const persistConfig={
